5c1aa40bcabdce4801f69a640cff982a9854a456
elciak82 <elciak82@gmail.com>
2023-11-09 19:51:57 +0100

    Refactor

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,22 +1,25 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Pulpit tests', () => {
-  test('quick payment with correct data', async ({ page }) => {
-    //Arrange
+  test.beforeEach(async ({ page }) => {
     const url = 'https://demo-bank.vercel.app/';
     const userId = 'testerLO';
     const userPassword = '10987654';
+
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+  });
+
+  test('quick payment with correct data', async ({ page }) => {
+    //Arrange
     const receiverId = '2';
     const transferAmount = '150';
     const transferTitle = 'pizza';
     const expectedTransverReceiver = 'Chuck Demobankowy';
 
     //Act
-    await page.goto(url);
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
-
     await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
     await page.locator('#widget_1_transfer_amount').fill(transferAmount);
     await page.locator('#widget_1_transfer_title').fill(transferTitle);
@@ -29,28 +32,22 @@ test.describe('Pulpit tests', () => {
       `Przelew wykonany! ${expectedTransverReceiver} - ${transferAmount},00PLN - ${transferTitle}`,
     );
   });
-});
 
-test('succesful mobile top-up', async ({ page }) => {
-  //Arrange
-  const url = 'https://demo-bank.vercel.app/';
-  const userId = 'testerLO';
-  const userPassword = '10987654';
-  const receiverOption = '500 xxx xxx';
-  const amount = '123';
+  test('succesful mobile top-up', async ({ page }) => {
+    //Arrange
+    const receiverOption = '500 xxx xxx';
+    const amount = '123';
 
-  await page.goto(url);
-  await page.getByTestId('login-input').fill(userId);
-  await page.getByTestId('password-input').fill(userPassword);
-  await page.getByTestId('login-button').click();
+    //Act
+    await page.locator('#widget_1_topup_receiver').selectOption(receiverOption);
+    await page.locator('#widget_1_topup_amount').fill(amount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
 
-  await page.locator('#widget_1_topup_receiver').selectOption(receiverOption);
-  await page.locator('#widget_1_topup_amount').fill(amount);
-  await page.locator('#uniform-widget_1_topup_agreement span').click();
-  await page.getByRole('button', { name: 'doładuj telefon' }).click();
-  await page.getByTestId('close-button').click();
-
-  await expect(page.locator('#show_messages')).toHaveText(
-    `Doładowanie wykonane! ${amount},00PLN na numer ${receiverOption}`,
-  );
+    //Assert
+    await expect(page.locator('#show_messages')).toHaveText(
+      `Doładowanie wykonane! ${amount},00PLN na numer ${receiverOption}`,
+    );
+  });
 });
