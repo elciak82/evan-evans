b73dd74b589ca7b84b46b1d2d7eac8bee32638c8
elciak82 <elciak82@gmail.com>
2023-12-03 23:50:01 +0100

    refactor booking component

--- tests/components/booking.component.ts
+++ tests/components/booking.component.ts
@@ -2,28 +2,27 @@ import { Page } from '@playwright/test';
 
 export const BookingComponent = (page: Page) => {
   const addAdult = page.getByRole('button', { name: '+' }).first();
-  const substractAdult = page.getByRole('button', { name: '-' }).first();
+  const subtractAdult = page.getByRole('button', { name: '-' }).first();
   const addChild = page.getByRole('button', { name: '+' }).nth(1);
-  const substractChild = page.getByRole('button', { name: '-' }).nth(1);
+  const subtractChild = page.getByRole('button', { name: '-' }).nth(1);
   const timeSlot = page.getByText('09:');
   const addToBasketButton = page.getByRole('button', { name: 'Add to basket' });
-  const today =  page.locator('[class*="selected"]');
+  const firstAvailableDate = page.locator('[class*="selected"]');
   const currentMonth = page.locator('.clndr__month');
   const dates = page.$$('[class*="calendar-dow-"]');
-
+  const bookingModal = page.locator('.modal-content');
 
   const selectDate = async (date: string): Promise<void> => {
-    for(const dt of await dates)
-    {
-      if(await dt.textContent()==date){
+    for (const dt of await dates) {
+      if ((await dt.textContent()) == date) {
         await dt.click();
         break;
       }
     }
-  }
-
+  };
 
   const addAdultClick = async (): Promise<void> => {
+    await page.waitForLoadState();
     await addAdult.click();
   };
 
@@ -33,27 +32,29 @@ export const BookingComponent = (page: Page) => {
 
   const selectTimeSlot = async (): Promise<void> => {
     await timeSlot.click();
-  }
+  };
 
-  const selectToday = async (): Promise<void> => {
-    await today.click();
-  }
+  const selectFirstAvailableDate = async (): Promise<void> => {
+    await firstAvailableDate.click();
+  };
 
   const addToBasketButtonClick = async (): Promise<void> => {
     await addToBasketButton.click();
-  }
+  };
 
   const bookTour = async (day: string): Promise<void> => {
     await addAdultClick();
-      if(day=='today')
-      {
-        await selectToday();
-      } else {
-        await selectDate(day)
-      }
+    await selectDate(day);
     await selectTimeSlot();
     await addToBasketButtonClick();
-  }
+  };
 
-  return { bookTour };
+  const bookTourForFirstAvailableDate = async (): Promise<void> => {
+    await addAdultClick();
+    await selectFirstAvailableDate();
+    await selectTimeSlot();
+    await addToBasketButtonClick();
+  };
+
+  return { bookTour, bookTourForFirstAvailableDate };
 };
