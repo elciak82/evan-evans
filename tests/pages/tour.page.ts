896be3ac26954f833392151e6f52693a98c0c164
elciak82 <elciak82@gmail.com>
2023-12-03 23:48:46 +0100

    waitForLoadState added to TourPage

--- tests/pages/tour.page.ts
+++ tests/pages/tour.page.ts
@@ -4,6 +4,7 @@ export const TourPage = (page: Page) => {
   const bookButton = page.getByRole('button', { name: 'Book now' });
 
   const bookButtonClick = async (): Promise<void> => {
+    await page.waitForLoadState();
     await bookButton.click();
   };
 
