ed5a70c2839f9f7e6e519966bc8dae21414ea8c6
elciak82 <elciak82@gmail.com>
2023-12-03 23:49:03 +0100

    added basket page

---
+++ tests/pages/basket.page.ts
@@ -0,0 +1,13 @@
+import { Page } from '@playwright/test';
+
+export const BasketPage = (page: Page) => {
+  const tourTitle = page.locator('[class*="card__title--slim"]');  
+
+  const getTourTitle = async () => {
+    await page.waitForLoadState();
+    console.log(await tourTitle.innerText());
+    return await tourTitle.innerText();
+  };
+
+  return { getTourTitle };
+};
