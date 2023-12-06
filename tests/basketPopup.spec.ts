59b0fd6e1390130758552b161417acd2de3fada9
elciak82 <elciak82@gmail.com>
2023-12-06 16:02:48 +0100

    added basketPopup.spec

---
+++ tests/basketPopup.spec.ts
@@ -0,0 +1,71 @@
+import { test, expect } from '@playwright/test';
+import { HomePage } from './pages/home.page';
+import { SearchPage } from './pages/search.page';
+import { TourPage } from './pages/tour.page';
+import { BookingComponent } from './components/booking.component';
+import { BasketComponent } from './components/basket.component';
+import { Alerts } from './helpers/enums/alerts.enums';
+import { Tours } from './helpers/enums/tours.enums';
+import { BasketPage } from './pages/basket.page';
+import { Persons } from './helpers/enums/persons.enums';
+
+test.describe('Booking - verifying data in the basket popup', () => {
+  let homePage: {
+    acceptCookie: any;
+    searchButtonClick: any;
+    inputTextToSearchField: any;
+  };
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    homePage = HomePage(page);
+    await homePage.acceptCookie();
+  });
+
+  test('Booking a trip for ONE CHILD and TWO ADULTS - checking data in the basket popup', async ({
+    page,
+  }) => {
+    //Arrange
+    const searchPage = SearchPage(page);
+    const tourPage = TourPage(page);
+    const booking = BookingComponent(page);
+    const basketPopup = BasketComponent(page);
+    const basketPage = BasketPage(page);
+
+    //Act
+    await homePage.inputTextToSearchField(Tours.KatowiceTour);
+    await homePage.searchButtonClick();
+
+    await searchPage.viewMoreButtonClick();
+    await tourPage.bookButtonClick();
+    await booking.fillBookingModal(Persons.ADULT, Persons.ADULT, Persons.CHILD);
+
+    const bookingDateTimeFromModal =
+      await booking.getBookingDateAndTimeFromModal();
+    const bookingAdultFromModal = await booking.adultBasketPrice();
+    const bookingChildFromModal = await booking.childBasketPrice();
+    const bookingTotalPriceFromModal =
+      await booking.getBookingTotalPriceFromModal();
+
+    await booking.addToBasketButtonClick();
+
+    //Assert
+    const itemAddedMessage = await basketPopup.getMessageText();
+    expect(itemAddedMessage).toBe(Alerts.ITEM_ADDED_BASKET_ALERT);
+
+    const tourInBasket = await basketPopup.getTourTitle();
+    expect(tourInBasket).toBe(Tours.KatowiceTour);
+
+    const basketDetails = await basketPopup.getBasketDetails();
+    expect(basketDetails.date).toContain(bookingDateTimeFromModal);
+    expect(basketDetails.persons[0]).toContain(bookingAdultFromModal);
+    expect(basketDetails.persons[1]).toContain(bookingChildFromModal);
+    expect(basketDetails.price).toContain(bookingTotalPriceFromModal);
+
+    //Clear
+    basketPopup.viewBasketButtonClick
+    await basketPage.removeTourFromBasket();
+    const removedItemAlert = await basketPage.getRemoverItemAlertText();
+    expect(Alerts.ITEM_REMOVED_BASKET_ALERT).toBe(removedItemAlert);
+  });
+});
