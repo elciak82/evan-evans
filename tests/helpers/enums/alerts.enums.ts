069c35fd0311135d1333b0eea262e4f7e012e149
elciak82 <elciak82@gmail.com>
2023-12-07 15:16:53 +0100

    alert enums added

--- tests/helpers/enums/alerts.enums.ts
+++ tests/helpers/enums/alerts.enums.ts
@@ -4,4 +4,9 @@ export enum Alerts {
     "We'll hold your item for 59 minutes",
   ITEM_REMOVED_BASKET_ALERT = 'Product has been successfully removed from your basket',
   INVALID_CODE = 'Invalid code',
+  VALID_NAME_ERROR = 'Please enter a valid name',
+  VALID_EMAIL_ERROR = 'Please enter a valid email',
+  VALID_PHONE_NUMBER_ERROR = 'Please enter a valid phone number',
+  VALID_COUNTRY_ERROR = 'Please enter a valid country',
+  ACCEPT_TERMS_AND_CONDITION_ERROR = 'Please confirm you accept terms and conditions',
 }
