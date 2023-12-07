3cdacb98f68eb1e3e37e57baf6d413d7d1bcb479
elciak82 <elciak82@gmail.com>
2023-12-07 09:35:58 +0100

    added user data

--- tests/test-data/login.data.ts
+++
@@ -1,5 +0,0 @@
-
-export const loginData = {
-  userId: 'testerLO',
-  userPassword: '10987654',
-};
---
+++ tests/test-data/userData.data.ts
@@ -0,0 +1,8 @@
+
+export const userData = {
+  firstName: 'FirstName',
+  lastName: 'LastName',
+  yourEmail: 'yourEmail@email.com',
+  yourPhoneNumber: '123456789',
+  yourCountry: 'Poland'
+};
