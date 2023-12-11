910e29926472e7dca2df7d2f6df96894bbcac5de
elciak82 <elciak82@gmail.com>
2023-12-11 10:00:57 +0100

    workers changed

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  // workers: process.env.CI ? 1 : undefined, //core
-  workers: 1, //change to above if you have stronger machine
+  workers: process.env.CI ? 1 : undefined, //core
+  // workers: 1, //change to above if you have stronger machine
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -73,11 +73,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
