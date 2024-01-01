29bcbe2d137aedfd79962886b12f32573c5a7532
elciak82 <elciak82@gmail.com>
2024-01-01 18:47:47 +0100

    userData model added

---
+++ src/models/userData.model.ts
@@ -0,0 +1,11 @@
+export type UserData = {
+    firstName: string,
+    lastName: string,
+    email: string,
+    phone: string,
+    country: string,
+    street1: string,
+    street2: string,
+    city: string,
+    zipCode: string,
+  };
